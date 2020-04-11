56a4f410c881deff0136abb4d4a1c259d12ede2c
noor-kherreh <54147857+noor-kherreh@users.noreply.github.com>
2020-04-11 09:19:12 +0500

    Update GeneLocationExtractor.java

--- src/input/GeneLocationExtractor.java
+++ src/input/GeneLocationExtractor.java
@@ -1,53 +1,58 @@
-package input;
+package input; //package input
 
+//include java packages
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.util.Properties;
 
+//include javax packages
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 
-import org.xml.sax.SAXException;
+//include SAXException
+import org.xml.sax.SAXException; //used for SAX exception, SAX mean Simple Api for XML
 
+//including some other package
 import tools.DASHandler;
 
 /*
  * By given the chromosome, start index and end index, GeneLocationExtractor returns the DNA sequence.
  * 
  */
-public class GeneLocationExtractor {
+public class GeneLocationExtractor { //main class
     private String chr;
     private int start_index;
     private int end_index;
     private String result_seq;
     private String DNA_database_prefix;
 
+    //parameterized constructor
     GeneLocationExtractor(String chr, int start_index, int end_index, Properties props)
 	    throws ParserConfigurationException, SAXException, IOException {
 	this.chr = chr;
 	this.start_index = start_index;
 	this.end_index = end_index;
-	this.DNA_database_prefix = props.getProperty("DNA_database_prefix");
+	this.DNA_database_prefix = props.getProperty("DNA_database_prefix");//getProperty: search the value against the given key in paramter
 	//GetDNAseq();
     }
 
     public String GetDNAseq() throws ParserConfigurationException, SAXException{
 	// Create a stream to hold the output
 	DASHandler dh = new DASHandler();
-	ByteArrayOutputStream baos = new ByteArrayOutputStream();
-	PrintStream ps = new PrintStream(baos);
+	ByteArrayOutputStream baos = new ByteArrayOutputStream();//This class implements an output stream in which the data is written into a byte array. The buffer automatically grows as data is written to it.
+	PrintStream ps = new PrintStream(baos); //printStream for writing/printing
 	// IMPORTANT: Save the old System.out!
-	PrintStream old = System.out;
+	PrintStream old = System.out; //Stream for writing to console - standard output stream
 	// Tell Java to use your special stream
-	System.setOut(ps);
-	SAXParserFactory f = SAXParserFactory.newInstance();
-	SAXParser parser = f.newSAXParser();
+	System.setOut(ps); //Now, from console it is changed to printStream object ps, which is basically our bytearray
+	SAXParserFactory f = SAXParserFactory.newInstance(); //Defines a factory API that enables applications to configure and obtain a SAX based parser to parse XML documents.
+	SAXParser parser = f.newSAXParser(); //Creates a new instance of a SAXParser using the currently configured factory parameters.
 	//System.out.println(DNA_database_prefix + this.chr + ":" + this.start_index + "," + this.end_index);
 	try {
-	    parser.parse(DNA_database_prefix + this.chr + ":" + this.start_index + "," + this.end_index, dh);
-	    System.out.flush();
+	    parser.parse(DNA_database_prefix + this.chr + ":" + this.start_index + "," + this.end_index, dh); //see at end
+	    System.out.flush(); 
 	} catch (IOException | SAXException e) {
 	    // TODO Auto-generated catch block
 	    System.setOut(old);
@@ -67,3 +72,17 @@ public class GeneLocationExtractor {
 	return result_seq;
     }
 }
+/*
+-> public void parse(String uri, DefaultHandler dh) throws SAXException, IOException
+
+Parse the content described by the giving Uniform Resource Identifier (URI) as XML using the specified DefaultHandler.
+
+Parameters:
+uri - The location of the content to be parsed.
+dh - The SAX DefaultHandler to use.
+
+Throws:
+IllegalArgumentException - If the uri is null.
+IOException - If any IO errors occur.
+SAXException - If any SAX errors occur during processing.
+*/
